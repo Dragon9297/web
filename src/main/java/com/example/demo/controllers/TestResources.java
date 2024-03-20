fa5bb07d98d286afb625757f1daa3a7ae5097df5
Dragon9297 <pankivannamaria92@gmail.com>
2024-03-20 19:37:56 +0200

    test2

--- src/main/java/com/example/demo/controllers/TestResources.java
+++ src/main/java/com/example/demo/controllers/TestResources.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
