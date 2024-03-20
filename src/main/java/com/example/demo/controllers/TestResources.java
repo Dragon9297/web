78724138afd3dc9a92e256733d21561a1ec126c2
Dragon9297 <pankivannamaria92@gmail.com>
2024-03-20 19:32:15 +0200

    test

--- src/main/java/com/example/demo/controllers/TestResources.java
+++ src/main/java/com/example/demo/controllers/TestResources.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @RestController
 public class TestResources {
-
 
     @GetMapping("resources/{name}")
     public String getName(@PathVariable String name){ return "Hello " + name;}
